95302b744bbe2952078c5c3acb1b5af1840b091c
Yohan Robert <yohan.robert@outlook.fr>
2015-11-15 00:59:54 +0100

    Changed bluetooth icon to have transparency

--- src/bluetooth.c
+++ src/bluetooth.c
@@ -16,13 +16,14 @@ static void bt_handler(bool connected){
 }
 
 void init_bluetooth_layer(Layer * root_layer){
-  s_icn_bt_black = gbitmap_create_with_resource(RESOURCE_ID_ICN_BT_BLACK);
+  s_icn_bt_black = gbitmap_create_with_resource(RESOURCE_ID_ICN_BLUETOOTH);
   GRect root_layer_bounds = layer_get_bounds(root_layer);
   GPoint layer_origin = grect_center_point(&root_layer_bounds);
   layer_origin.x -= ICN_BT_WIDTH / 2;
   layer_origin.y -= ICN_BT_HEIGHT / 2 + ICN_BT_RADIUS;
   GRect layer_bounds = (GRect) { .origin = layer_origin, .size = GSize(ICN_BT_WIDTH, ICN_BT_HEIGHT) };
   s_bluetooth_layer = bitmap_layer_create(layer_bounds);
+  bitmap_layer_set_compositing_mode(s_bluetooth_layer, GCompOpSet);
   bitmap_layer_set_bitmap(s_bluetooth_layer, s_icn_bt_black);
   bt_handler(connection_service_peek_pebble_app_connection());
   layer_add_child(root_layer, bitmap_layer_get_layer(s_bluetooth_layer));
