536ffe2712597d7462b4cc6ab53ead52d1cfe0a7
Yohan Robert <yohan.robert@outlook.fr>
2015-12-21 21:04:14 +0400

    Changed bluetooth icon bmp to font

--- src/bluetooth.c
+++ src/bluetooth.c
@@ -1,3 +1,4 @@
+
 #include <pebble.h>
 #include "bluetooth.h"
 #include "config.h"
@@ -5,58 +6,54 @@
 
 static const int ICON_OFFSET = -18;
 
-static GBitmap * s_icn_bt;
-static GBitmap * s_icn_heart;
-static BitmapLayer * s_bt_layer;
-static BitmapLayer * s_heart_layer;
+static TextLayer * s_bt_layer;
 static Layer * s_root_layer;
 
-static void hide_layer(const BitmapLayer * layer, const bool hidden){
-  layer_set_hidden(bitmap_layer_get_layer(layer), hidden);
-}
-
 static void bt_handler(bool connected){
+  const GColor bg_color = config_get_background_color();
+  bool bg_reddish = false;
+  const GColor reddish_colors[] = { GColorRed, GColorFolly, GColorFashionMagenta, GColorMagenta };
+  for(int i = 0; i < 4; i++){
+    if(gcolor_equal(bg_color, reddish_colors[i])){
+      bg_reddish = true;
+      break;
+    }
+  }
+  if(bg_reddish){
+    text_layer_set_text_color(s_bt_layer, GColorWhite);
+  }else{
+    text_layer_set_text_color(s_bt_layer, GColorRed);
+  }
   const BluetoothIcon new_icon = config_get_bluetooth_icon();
   if(connected || new_icon == NoIcon){
-    hide_layer(s_bt_layer, true);
-    hide_layer(s_heart_layer, true);
+    text_layer_set_text(s_bt_layer, "");
+  }else if(new_icon == Bluetooth){
+    text_layer_set_text(s_bt_layer, "μ");
   }else{
-    hide_layer(s_bt_layer, new_icon != Bluetooth);
-    hide_layer(s_heart_layer, new_icon != Heart);
+    text_layer_set_text(s_bt_layer, "ν");
   }
-}
-
-static GRect icon_bounds(const GPoint center, const GBitmap * bitmap){
-  const GSize size = gbitmap_get_bounds(bitmap).size;
-  const GRect rect_at_center = (GRect) { .origin = center, .size = size };
-  return grect_translated(rect_at_center, - size.w / 2, - size.h + ICON_OFFSET);
-}
-
-static void setup_bitmap(GBitmap ** bitmap, BitmapLayer ** layer, const int key){
-  const GRect root_layer_bounds = layer_get_bounds(s_root_layer);
-  const GPoint center = grect_center_point(&root_layer_bounds);
-  *bitmap = gbitmap_create_with_resource(key);
-  const GRect bounds = icon_bounds(center, *bitmap);
-  *layer = bitmap_layer_create(bounds);
-  bitmap_layer_set_bitmap(*layer, *bitmap);
-  bitmap_layer_set_compositing_mode(*layer, GCompOpSet);
-  layer_add_child(s_root_layer, bitmap_layer_get_layer(*layer));
 }
 
 void init_bluetooth_layer(Layer * root_layer){
   s_root_layer = root_layer;
-  setup_bitmap(&s_icn_bt, &s_bt_layer, RESOURCE_ID_ICN_BLUETOOTH);
-  setup_bitmap(&s_icn_heart, &s_heart_layer, RESOURCE_ID_ICN_HEART);
+  const GRect root_layer_bounds = layer_get_bounds(root_layer);
+  const GPoint center = grect_center_point(&root_layer_bounds);
+  const GSize size = GSize(23, 23);
+  const GRect rect_at_center = (GRect) { .origin = center, .size = size };
+  const GRect bounds = grect_translated(rect_at_center, - size.w / 2, - size.h + ICON_OFFSET);
+  s_bt_layer = text_layer_create(bounds);
+  text_layer_set_text_alignment(s_bt_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_bt_layer, get_font());
+  text_layer_set_overflow_mode(s_bt_layer, GTextOverflowModeWordWrap);
+  text_layer_set_background_color(s_bt_layer, GColorClear);
+  layer_add_child(root_layer, text_layer_get_layer(s_bt_layer));
   bluetooth_connection_service_subscribe(bt_handler);
-  bt_handler(connection_service_peek_pebble_app_connection());
+  mark_dirty_bluetooth_layer();
 }
 
 void deinit_bluetooth_layer(){
   bluetooth_connection_service_unsubscribe();
-  bitmap_layer_destroy(s_heart_layer);
-  gbitmap_destroy(s_icn_heart);
-  bitmap_layer_destroy(s_bt_layer);
-  gbitmap_destroy(s_icn_bt);
+  text_layer_destroy(s_bt_layer);
 }
 
 void mark_dirty_bluetooth_layer(){
