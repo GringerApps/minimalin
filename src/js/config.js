9f2bfad6807608f914e0423b5efb96d18e0a2e62
Yohan Robert <yohan.robert@outlook.fr>
2016-10-20 16:03:41 +0200

    Update config page url

--- src/js/config.js
+++ src/js/config.js
@@ -27,7 +27,7 @@ Pebble.addEventListener('showConfiguration', function() {
     return localStorage.getItem(attr);
   }
 
-  var url = 'https://cdn.rawgit.com/groyoh/minimalin/a3ceb112f397d3853cac544523a3073c26231711/config/index.html?';
+  var url = 'https://cdn.rawgit.com/groyoh/minimalin/129af4fe69d4d48336837448c3563300bd8f4359/config/index.html?';
   var params = {
     minute_hand_color: getSavedColor('MinuteHand'),
     hour_hand_color: getSavedColor('HourHand'),
