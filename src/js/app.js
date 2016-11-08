7a64598c14551b8dd6adf1676bc7c56b349af2c0
Yohan Robert <yohan.robert@outlook.fr>
2016-11-08 12:39:58 +0100

    Update config page URL

--- src/js/app.js
+++ src/js/app.js
@@ -121,7 +121,7 @@ Pebble.addEventListener('ready', function (e) {
 
 
 Pebble.addEventListener('showConfiguration', function() {
-  var URL = 'https://cdn.rawgit.com/groyoh/minimalin/a5d055e3d66bc6ed457f2bdc2345ddb97785b097/config/index.html';
+  var URL = 'https://cdn.rawgit.com/groyoh/minimalin/11fdd010d6ac0005d03712082098ce3d5b5bb5fe/config/index.html';
   var config = Config('config');
   var params = config.load();
   params.platform = Pebble.getActiveWatchInfo().platform;
